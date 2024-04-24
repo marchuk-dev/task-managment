09688f99215777e4818baf22c0ebfb9f5ade550c
Mykhailo Marchuk <mishaakamichael999@gmail.com>
2024-04-24 12:28:37 +0300

    Add sql scripts for creating and initializing

--- src/main/resources/db/migration/V2__init_task.sql
+++ src/main/resources/db/migration/V2__init_task.sql
@@ -1,9 +1,9 @@
-INSERT INTO task (id, title, description, status)
-VALUES (1, 'Complete Project Proposal', 'Write a detailed proposal for the upcoming project deadline', 'IN_PROGRESS'),
-       (2, 'Review Code Changes', 'Review and provide feedback on the latest code changes in the development branch',
+INSERT INTO task (title, description, status)
+VALUES ('Complete Project Proposal', 'Write a detailed proposal for the upcoming project deadline', 'IN_PROGRESS'),
+       ('Review Code Changes', 'Review and provide feedback on the latest code changes in the development branch',
         'PENDING'),
-       (3, 'Client Meeting Preparation',
-        'Prepare presentation materials and agenda for the client meeting scheduled next week', 'TODO'),
-       (4, 'Bug Fixing', 'Investigate and fix the critical bug reported by the QA team', 'IN_PROGRESS'),
-       (5, 'Deploy Application Update', 'Deploy the latest application update to the staging environment for testing',
+       ('Client Meeting Preparation',
+        'Prepare presentation materials and agenda for the client meeting scheduled next week', 'TO_DO'),
+       ('Bug Fixing', 'Investigate and fix the critical bug reported by the QA team', 'IN_PROGRESS'),
+       ('Deploy Application Update', 'Deploy the latest application update to the staging environment for testing',
         'PENDING');
