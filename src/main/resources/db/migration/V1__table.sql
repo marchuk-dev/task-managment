d305827cbde6c838f8db971502677024467ccf6c
Mykhailo Marchuk <mishaakamichael999@gmail.com>
2024-04-24 12:28:37 +0300

    Add sql scripts for creating and initializing

--- src/main/resources/db/migration/V1__table.sql
+++ src/main/resources/db/migration/V1__table.sql
@@ -1,8 +1,9 @@
+CREATE TYPE "task_status_type" AS ENUM ('PENDING','COMPLETED','IN_PROGRESS','TO_DO');
 CREATE TABLE task
 (
     id          BIGSERIAL NOT NULL,
     title       VARCHAR(50),
     description VARCHAR(255),
-    status      VARCHAR(255),
+    status      task_status_type,
     CONSTRAINT pk_tasks PRIMARY KEY (id)
 );
