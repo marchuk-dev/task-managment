388e0488f681f44c36fdb01fa6cc7a4298429156
Mykhailo Marchuk <mishaakamichael999@gmail.com>
2024-04-24 23:44:21 +0300

    Update TaskController

--- src/main/java/com/miratech/miratechtechtask/controllers/TaskController.java
+++ src/main/java/com/miratech/miratechtechtask/controllers/TaskController.java
@@ -2,6 +2,7 @@ package com.miratech.miratechtechtask.controllers;
 
 import com.miratech.miratechtechtask.dto.TaskDto;
 import com.miratech.miratechtechtask.dto.TaskStatus;
+import com.miratech.miratechtechtask.dto.UpdateStatusDto;
 import com.miratech.miratechtechtask.service.TaskManagementService;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
@@ -13,6 +14,9 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -40,7 +44,8 @@ public class TaskController {
                                 @Min(1) @RequestParam(required = false, defaultValue = "10") @Valid Integer offset,
                                 String title,
                                 @RequestParam(required = false) String status) {
-        Pageable pageable = PageRequest.of(page, offset);
+        Pageable pageable = PageRequest.of(page, offset,
+                Sort.by("title"));
         return taskService.getAll(pageable, title, status);
     }
 
@@ -61,18 +66,19 @@ public class TaskController {
     }
 
     /**
-     * Creates a new task.
+     * Create a new task.
      *
-     * @param dto The TaskDto object representing the task to create.
-     * @return The TaskDto object representing the newly created task.
+     * @param dto The TaskDto containing the details of the task to be created.
+     * @return ResponseEntity<TaskDto> containing the created task if successful, with HTTP status code 201 (Created).
      */
     @ApiOperation("Create a new task")
     @ApiResponses({
             @ApiResponse(code = 201, message = "Task created successfully")
     })
     @PostMapping("tasks")
-    public TaskDto create(@RequestBody TaskDto dto) {
-        return taskService.create(dto);
+    public ResponseEntity<TaskDto> create(@RequestBody TaskDto dto) {
+        TaskDto createdTask = taskService.create(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdTask);
     }
 
     /**
@@ -89,14 +95,14 @@ public class TaskController {
     })
     @PutMapping("tasks/{id}")
     public TaskDto updateById(@PathVariable Long id, @RequestBody TaskDto dto) {
-        return taskService.update(dto);
+        return taskService.update(id, dto);
     }
 
     /**
      * Update the status of a task by its ID.
      *
-     * @param id     The ID of the task to update.
-     * @param status The new status of the task.
+     * @param id        The ID of the task to update.
+     * @param statusDto The new status of the task.
      * @return The updated TaskDto representing the task with the new status.
      */
     @ApiOperation("Update task status by ID")
@@ -105,8 +111,8 @@ public class TaskController {
             @ApiResponse(code = 404, message = "Task with the specified ID not found")
     })
     @PatchMapping("tasks/{id}")
-    public TaskDto updateStatus(@PathVariable Long id, @RequestBody TaskStatus status) {
-        return taskService.updateStatus(id, status);
+    public TaskDto updateStatus(@PathVariable Long id, @RequestBody UpdateStatusDto statusDto) {
+        return taskService.updateStatus(id, TaskStatus.fromStatus(statusDto.status()));
     }
 
     /**
