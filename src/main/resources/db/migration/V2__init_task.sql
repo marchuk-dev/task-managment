2408b438feb95c71e64c235efd3772dde2d7beb2
Mykhailo Marchuk <mishaakamichael999@gmail.com>
2024-04-25 10:20:48 +0300

    Add more init data

--- src/main/resources/db/migration/V2__init_task.sql
+++ src/main/resources/db/migration/V2__init_task.sql
@@ -6,4 +6,12 @@ VALUES ('Complete Project Proposal', 'Write a detailed proposal for the upcoming
         'Prepare presentation materials and agenda for the client meeting scheduled next week', 'TO_DO'),
        ('Bug Fixing', 'Investigate and fix the critical bug reported by the QA team', 'IN_PROGRESS'),
        ('Deploy Application Update', 'Deploy the latest application update to the staging environment for testing',
-        'PENDING');
+        'PENDING'),
+       ('Prepare Project Proposal', 'Draft a detailed proposal outlining project scope and objectives', 'IN_PROGRESS'),
+       ('Code Review for Feature X', 'Conduct a thorough review of the code changes related to Feature X', 'PENDING'),
+       ('Client Meeting Agenda Creation',
+        'Create an agenda for the upcoming client meeting to ensure smooth discussions', 'TO_DO'),
+       ('Critical Bug Resolution',
+        'Identify and resolve critical bugs reported by the QA team to ensure product stability', 'IN_PROGRESS'),
+       ('Staging Environment Deployment',
+        'Deploy the latest application update to the staging environment for testing and validation', 'PENDING');
